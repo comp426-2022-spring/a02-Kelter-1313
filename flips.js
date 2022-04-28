d782fd004b103b992148f33141a264c43d40070e
Albert "Christian" DeMarco <Kelter-1313>
2022-04-28 14:58:51 -0400

    plsWork

--- flips.js
+++ flips.js
@@ -6,14 +6,17 @@ import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 
 
 if (process.argv.length < 3){
-    throw new Error("Error: no input\nUsage: node flips --number=9")
+    console.error("Error: no input\nUsage: node flips --number=9")
+    process.exit(1)
 }else{
 const args = process.argv.slice(2);
 if (args[0][9] == null){
- //   console.error("Error: no input")
-    throw new Error("Usage: node flips --number=9")
+    console.error("Error: no input\nUsage: node flips --number=9")
+    process.exit(1)
+    
 }else if (args[0][9] != 0 | args[0][9] != 1 && args[0][9] != 2&& args[0][9] != 3&& args[0][9] != 4&& args[0][9] != 5&& args[0][9] != 6&& args[0][9] != 7&& args[0][9] != 8&& args[0][9] != 9){
-    throw new Error("Usage: node flips --number=9")
+    console.error("Usage: node flips --number=9")
+    process.exit(1)
 }else{
 var resultArray = coinFlips(args[0].slice(9))
 
--- guess-flip.js
+++ guess-flip.js
@@ -2,15 +2,17 @@ import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 
 if (process.argv.length < 3){
     // return error message
-    throw new Error("Error: no input.\nUsage: node guess-flip --call=heads|")
-  //  process.exit(1)
+    console.error("Error: no input.\nUsage: node guess-flip --call=heads|")
+    process.exit(1)
 }else{
 var args = process.argv[2];
 if(args == "--call="){
-    throw new Error("Error: no input.\nUsage: node guess-flip --call=heads")
+    console.error("Error: no input.\nUsage: node guess-flip --call=heads")
+    process.exit(1)
 }else if(((args != "--call=heads") && (args != "--call=tails")) || args === null){
     // return error message
-    throw new Error("Usage: node guess-flip --call=tails")
+    console.error("Usage: node guess-flip --call=tails")
+    process.exit(1)
 
 }else{console.log(flipACoin(args.slice(7)))}
 }
