dc6abe489c07c286e62c33acee3e3a8a38d11393
Albert "Christian" DeMarco <Kelter-1313>
2022-04-28 00:43:40 -0400

    Update flips.js

--- flips.js
+++ flips.js
@@ -7,10 +7,11 @@ import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 
 if (process.argv.length < 3){
     console.error("Error: no input")
+    console.error("Usage: node flips --number=9")
 }else{
 const args = process.argv.slice(2);
 if (args[0][9] == null){
-    console.error("Error: no input")
+ //   console.error("Error: no input")
     console.error("Usage: node flips --number=9")
 }else if (args[0][9] != (1|2|3|4|5|6|7|8|9|0)){
     console.error("Usage: node flips --number=9")
