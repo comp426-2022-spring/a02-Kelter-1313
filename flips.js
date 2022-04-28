04c44ed79513b9a22458d100cad67dd7fcc7dc03
Albert "Christian" DeMarco <Kelter-1313>
2022-04-28 00:30:00 -0400

    Update flips.js

--- flips.js
+++ flips.js
@@ -7,9 +7,12 @@ import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 
 
 const args = process.argv.slice(2);
+if (args[0][9] == null){
+    console.error("Error: no input")
+}else{
 var resultArray = coinFlips(args[0].slice(9))
 
 console.log(resultArray);
 console.log(countFlips(resultArray))
-
+}
 
