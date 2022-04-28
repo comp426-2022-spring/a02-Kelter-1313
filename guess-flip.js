465bdca460a42ef58f7c16fe1e964f4cae04f3af
Albert "Christian" DeMarco <Kelter-1313>
2022-04-28 00:08:20 -0400

    Update guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -5,12 +5,12 @@ if (process.argv.length < 3){
     console.error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]")
   //  process.exit(1)
 }else{
-var args = process.argv.slice(2);
+var args = process.argv[2];
 if(args == "--call="){
     console.error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]")
 }else if(((args != "--call=heads") && (args != "--call=tails")) || args === null){
     // return error message
     console.error("Usage: node guess-flip --call=[heads|tails]")
 
-}else{console.log(flipACoin(args))}
+}else{console.log(flipACoin(args.slice(7)))}
 }
