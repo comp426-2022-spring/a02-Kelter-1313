3681f2df0a3ff2b586c1bffe91c63a27e35f1f98
Albert "Christian" DeMarco <Kelter-1313>
2022-04-28 00:42:04 -0400

    Update flips.js

--- flips.js
+++ flips.js
@@ -11,6 +11,9 @@ if (process.argv.length < 3){
 const args = process.argv.slice(2);
 if (args[0][9] == null){
     console.error("Error: no input")
+    console.error("Usage: node flips --number=9")
+}else if (args[0][9] != (1|2|3|4|5|6|7|8|9|0)){
+    console.error("Usage: node flips --number=9")
 }else{
 var resultArray = coinFlips(args[0].slice(9))
 
