e086f83d6bc2d0b59fadae409a7baff4f55f88a3
Albert "Christian" DeMarco <Kelter-1313>
2022-04-27 22:35:46 -0400

    Update guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -1,13 +1,13 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 if (process.argv.length < 3){
     // return error message
-    error("Must type either heads or tails")
+    throw new Error(error("Must type either heads or tails"))
     process.exit(1)
 }
 const args = process.argv.slice(2);
 if ((args[0] != "heads") && (args[0] != "tails")){
     // return error message
-    error("Must type either heads or tails")
+    throw new Error("Must type either heads or tails")
     
     process.exit(1)
 }
