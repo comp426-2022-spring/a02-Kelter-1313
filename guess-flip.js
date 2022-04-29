212ec7c6f1bdef03fe71a6b82503c5c65f0e012a
Albert "Christian" DeMarco <Kelter-1313>
2022-04-29 19:09:12 -0400

    guessMaybeFixed
    Just need multi now!

--- guess-flip.js
+++ guess-flip.js
@@ -1,12 +1,13 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 
+
 if (process.argv.length < 3){
     // return error message
     console.error("Error: no input.\nUsage: node guess-flip --call=heads|")
     process.exit(1)
 }else{
 var args = process.argv[2];
-if(args == "--call="){
+if(args == "--call=" || args == "--call"){
     console.error("Error: no input.\nUsage: node guess-flip --call=heads")
     process.exit(1)
 }else if(((args != "--call=heads") && (args != "--call=tails")) || args === null){
