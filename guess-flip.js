8bf355352dab9db2bef55a3df3f9b129f1ad906a
Albert "Christian" DeMarco <Kelter-1313>
2022-04-29 19:15:10 -0400

    Update guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -3,16 +3,19 @@ import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
 
 if (process.argv.length < 3){
     // return error message
-    console.error("Error: no input.\nUsage: node guess-flip --call=heads|")
+    console.error()
+   // console.error("Error: no input.\nUsage: node guess-flip --call=heads")
     process.exit(1)
 }else{
 var args = process.argv[2];
 if(args == "--call=" || args == "--call"){
-    console.error("Error: no input.\nUsage: node guess-flip --call=heads")
+    console.error()
+   // console.error("Error: no input.\nUsage: node guess-flip --call=heads")
     process.exit(1)
 }else if(((args != "--call=heads") && (args != "--call=tails")) || args === null){
     // return error message
-    console.error("Usage: node guess-flip --call=tails")
+    console.error()
+    //console.error("Usage: node guess-flip --call=tails")
     process.exit(1)
 
 }else{console.log(flipACoin(args.slice(7)))}
